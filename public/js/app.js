529cd25c0aae058d2645f4ee910b7e91cb174192
Prashant <prashantsa255@gmail.com>
2020-07-14 06:31:50 +0530

    heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 let sortDirection = false;
 
-const url = new URL("http://localhost:3000/users")
+const url = new URL("https://incablet-assign.herokuapp.com//users")
 
 fetch(url).then((response) => {
     response.json().then((data) => {
