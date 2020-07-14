ef498e39cf269b7e6700f68e9ca190a1eea8215e
Prashant <prashantsa255@gmail.com>
2020-07-14 06:34:32 +0530

    Error removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 let sortDirection = false;
 
-const url = new URL("https://incablet-assign.herokuapp.com//users")
+const url = new URL("https://incablet-assign.herokuapp.com/users")
 
 fetch(url).then((response) => {
     response.json().then((data) => {
